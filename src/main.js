582779e5ae0713cf2e30fb9614441d63ecfec6a3
lihaomin <lihaomin666@qq.com>
2020-04-01 11:39:22 +0800

    vant组件/rem适配/fastclick延迟

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 移动端适配
+import 'amfe-flexible'
+// 解决移动端300毫秒延迟
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 Vue.config.productionTip = false
 
 new Vue({
