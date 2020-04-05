d5ff9b6ea004786e92fd59bfbfcc749104d22703
lihaomin <lihaomin666@qq.com>
2020-04-05 10:48:43 +0800

    dplayer初版

--- src/views/home/offline-course/OfflineCourseDetail.vue
+++ src/views/home/offline-course/OfflineCourseDetail.vue
@@ -17,29 +17,75 @@
         阿就看到哈就开始的哈就睡得好说的那款经
         那不就是不对劲啊不大的哈就看到。
       </div>
-      <div class="introduction"></div>
+      <div class="introduction" @click="showController = !showController">
+        <d-player ref="player" @timeupdate="timeupdate" @ended="ended" :options="options"></d-player>
+        <div class="player-controller" v-if="showController">
+          <img @click.stop="play" :src="require('@/assets/images/home/v-play.png')" />
+        </div>
+      </div>
     </div>
     <div class="footer">
-      <van-button class="apply" type="default">我要报名</van-button>
+      <van-button class="apply" @click="apply" type="default">我要报名</van-button>
     </div>
   </div>
 </template>
 
 <script>
-import { Button } from 'vant'
+import { Button, Dialog } from 'vant'
 import NavBar from '@/components/nav-bar/NavBar'
+import VueDPlayer from 'vue-dplayer'
+import 'vue-dplayer/dist/vue-dplayer.css'
 
 export default {
   name: 'OfflineCourseDetail',
   components: {
     [Button.name]: Button,
-    NavBar
+    [Dialog.name]: Dialog,
+    NavBar,
+    'd-player': VueDPlayer
   },
   data () {
     return {
+      player: null,
+      showController: true,
+      options: {
+        video: {
+          url: 'http://cdn.toxicjohann.com/lostStar.mp4',
+          pic: 'https://i.loli.net/2020/04/04/Fed3fi4m8bGklsu.jpg'
+        },
+        preload: false,
+        screenshot: false,
+        autoplay: false,
+        contextmenu: []
+      }
     }
   },
   methods: {
+    // 视频播放
+    play () {
+      this.player.toggle()
+      this.showController = !this.showController
+    },
+    // 播放中
+    timeupdate () {
+      if (this.player.video.currentTime < 1) alert(this.player.video.currentTime)
+    },
+    // 视频播放结束
+    ended () {
+      // http://cdn.toxicjohann.com/lostStar.mp4
+      // http://vjs.zencdn.net/v/oceans.mp4
+      // this.player.switchVideo({
+      //   url: 'http://cdn.toxicjohann.com/lostStar.mp4'
+      alert('视频播放结束')
+      // })
+    },
+    // 我要报名
+    apply () {
+      alert('我要报名')
+    }
+  },
+  mounted () {
+    this.player = this.$refs.player.dp
   }
 }
 </script>
@@ -87,7 +133,7 @@ export default {
     padding: 10px;
     border-radius:10px;
     background-color: #F6FBFF;
-    overflow: hidden;
+    overflow: scroll;
   }
   .summary .introduction {
     width: 305px;
@@ -97,6 +143,20 @@ export default {
     background-color: #333;
     border-radius: 10px;
     opacity: 0.4;
+  }
+  .summary .player-controller {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
+  }
+  .summary .player-controller img {
+    width: 45px;
+    height: 45px;
   }
   .footer {
     display: flex;
@@ -109,5 +169,30 @@ export default {
     border-radius: 2px;
     margin: 10px 15px;
     background-color: #DB6073;
+  }
+  .dplayer {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+
+  .introduction >>> .dplayer-controller {
+    /* 底部控制条 */
+    display: none;
+  }
+
+  .introduction >>> .dplayer-notice {
+    /* 快退进提示 */
+    display: none;
+  }
+
+  .introduction >>> .dplayer-menu-show {
+    /* 右键信息 */
+    display: none !important;
+  }
+
+  .introduction >>> .dplayer-mask {
+    /* 悬浮层 */
+    display: inline-block;
   }
 </style>
