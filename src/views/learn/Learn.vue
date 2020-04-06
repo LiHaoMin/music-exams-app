a8b7f4980a02cc334acd359681eccfe28c53004b
lihaomin <lihaomin666@qq.com>
2020-04-06 15:43:10 +0800

    我的学习

--- src/views/learn/Learn.vue
+++ src/views/learn/Learn.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="learn">
-    <div>Learn</div>
+    <van-tabs v-model="currentIdx"
+              animated
+              sticky
+              color="#1E4058"
+              line-height="2"
+              title-inactive-color="#333"
+              title-active-color="#1E4058">
+      <van-tab title-style="font-size: 0.42667rem;font-weight:500;" title="未学完">
+        <UnlearnedTab />
+      </van-tab>
+      <van-tab title-style="font-size: 0.42667rem;font-weight:500;" title="已学完">
+        <LearnedTab />
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
+import { Tab, Tabs } from 'vant'
+import UnlearnedTab from '@/views/learn/UnlearnedTab'
+import LearnedTab from '@/views/learn/LearnedTab'
+
 export default {
   name: 'Learn',
   components: {
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
+    UnlearnedTab,
+    LearnedTab
+  },
+  data () {
+    return {
+      currentIdx: 0
+    }
   }
 }
 </script>
---
+++ src/views/learn/LearnedTab.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="learned-tab">
+    <div class="list-warp" v-if="!isEmpty">
+      <van-list
+        v-model="loading"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad">
+        <ListItemCard :isPageType="2" @onRateClick="onRateClick" :key="index" v-for="(item, index) in list" @onItemClick="$router.push('/course/detail')" />
+      </van-list>
+    </div>
+    <div class="list-empty" v-if="isEmpty">
+      <img :src="require('@/assets/images/learn/empty.png')" />
+      <p>这里空空如也～</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { List } from 'vant'
+import ListItemCard from '@/components/list/ListItemCard'
+
+export default {
+  name: 'LearnedTab',
+  components: {
+    [List.name]: List,
+    ListItemCard
+  },
+  data () {
+    return {
+      isEmpty: false,
+      list: [],
+      loading: false,
+      error: false,
+      finished: false,
+      isSearched: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+
+        // 加载状态结束
+        this.loading = false
+        this.isEmpty = false
+
+        if (this.list.length === 20) {
+          this.error = true
+          return
+        }
+
+        // 数据全部加载完成
+        if (this.list.length >= 30) {
+          this.finished = true
+        }
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .learned-tab {
+    min-height: 100vh;
+    background-color: #F8F8F8;
+  }
+
+  .list-warp {
+    padding: 10px 15px 50px 15px;
+  }
+
+  .list-empty {
+    width: 100%;
+    height: 80vh;
+    margin: 0 auto;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .list-empty p {
+    font-size: 14px;
+    font-weight: 400;
+    color: #888;
+    line-height: 20px;
+  }
+</style>
