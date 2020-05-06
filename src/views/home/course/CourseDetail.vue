14d2ceca23a008ae7a3e9a02ce3001ab20468fed
lihaomin <lihaomin666@qq.com>
2020-05-06 22:01:12 +0800

    需修改

--- src/views/home/course/CourseDetail.vue
+++ src/views/home/course/CourseDetail.vue
@@ -111,10 +111,54 @@ export default {
     this.requestCourseDetail()
     this.requestComment()
     this.requestChapter()
+    this.$http.get('/wx/fx', { params: { url: 'http://yinyuebojiangtang.com/' } }).then((res) => {
+      if (res && res.data) {
+        window.wx.config({
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: res.data.appId, // 必填，公众号的唯一标识
+          timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+          nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
+          signature: res.data.signature, // 必填，签名
+          jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
+        })
+      } else {
+        Toast.fail('操作失败')
+      }
+    })
   },
   methods: {
     // 分享
     share () {
+      // this.$http.get('/wx/fx', { params: { url: 'http://yinyuebojiangtang.com/' } }).then((res) => {
+      //   if (res && res.data) {
+      //     // const config = {
+      //     //   types: ['wx', 'wxline', 'qq', 'qzone']
+      //     // }
+      //     window.wx.config({
+      //       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      //       appId: res.data.appId, // 必填，公众号的唯一标识
+      //       timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+      //       nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
+      //       signature: res.data.signature, // 必填，签名
+      //       jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'] // 必填，需要使用的JS接口列表
+      //     })
+      //     // , 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone'
+      //     // mShare.wxConfig({
+      //     //   title: this.detail.curriculumName,
+      //     //   link: encodeURIComponent(window.location.href),
+      //     //   desc: this.detail.briefIntroduction,
+      //     //   imgUrl: this.detail.curriculumImg,
+      //     //   wx: {
+      //     //     appId: res.data.appId, // 必填，公众号的唯一标识
+      //     //     timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+      //     //     nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
+      //     //     signature: res.data.signature // 必填，签名
+      //     //   }
+      //     // })
+      //   } else {
+      //     Toast.fail('操作失败')
+      //   }
+      // })
       const config = {
         types: ['wx', 'wxline', 'qq', 'qzone']
       }
